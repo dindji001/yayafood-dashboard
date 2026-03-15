3f4046422f852d699b67aad2ec2dcf027f75e15e
dindji001 <severindindji68@gmail.com>
2026-03-15 21:57:22 +0000

    mettre a jou rle projet

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -8,6 +8,18 @@ class Dish extends Model
 {
     protected $fillable = ['category_id', 'name', 'description', 'price', 'image', 'is_available'];
 
+    protected $appends = ['image_url'];
+
+    protected $casts = [
+        'is_available' => 'boolean',
+        'price' => 'integer',
+    ];
+
+    public function getImageUrlAttribute()
+    {
+        return $this->image ? asset('storage/' . $this->image) : null;
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);

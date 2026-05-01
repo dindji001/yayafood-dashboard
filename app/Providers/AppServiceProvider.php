15d4266522588d685807611718b2318db8669540
dindji001 <severindindji68@gmail.com>
2026-05-01 14:37:04 +0000

    correction de burg de creation de restaurant

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,16 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Définir le répertoire temporaire de l'application
-        $tempDir = storage_path('tmp');
-        if (!is_dir($tempDir)) {
-            mkdir($tempDir, 0755, true);
-        }
-        putenv('TMPDIR=' . $tempDir);
-        putenv('TEMP=' . $tempDir);
-        putenv('TMP=' . $tempDir);
-        ini_set('upload_tmp_dir', $tempDir);
-        ini_set('sys_temp_dir', $tempDir);
+        //
     }
 
     /**

e25531e8d1a173f0ce605a9a488bb6e7fc619a41
dindji001 <severindindji68@gmail.com>
2026-05-01 15:06:31 +0000

    modifier le mot de passe du restaurant depuis le dahboard admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,17 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// Définir le répertoire temporaire AVANT de configurer l'application
-$tempDir = dirname(__DIR__) . '/storage/tmp';
-if (!is_dir($tempDir)) {
-    mkdir($tempDir, 0755, true);
-}
-putenv('TMPDIR=' . $tempDir);
-putenv('TEMP=' . $tempDir);
-putenv('TMP=' . $tempDir);
-@ini_set('upload_tmp_dir', $tempDir);
-@ini_set('sys_temp_dir', $tempDir);
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -28,10 +17,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        // Ignorer le notice spécifique sur tempnam()
-        $exceptions->report(function (ErrorException $e) {
-            if (str_contains($e->getMessage(), 'tempnam(): file created in the system's temporary directory')) {
-                return false;
-            }
-        });
+        //
     })->create();
